e46e914357965e031c999172724e058dd1e7e3a1
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-10 22:39:15 -0400

    start for next day

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+import turtle_blueprint
+screen = Screen()
+screen.screensize(200,200)
+
+# red_turtle = Turtle().color("red")
+# green_turtle = Turtle().color("green")
+# blue_turtle = Turtle().color("blue")
+# purple_turtle = Turtle().color("purple")
+#
+
+screen.exitonclick()
+
+#fixes are on the way
---
+++ turtle_blueprint.py
@@ -0,0 +1,10 @@
+import turtle
+from turtle import Turtle
+import random
+
+move_forward = random.randint(5,10)
+
+def turtle_move():
+    turtle.forward(move_forward)
+# def turtle_start(start):
+#     turtle.goto(start)
