5e16f9a5a997253050dd5c5de45c9d062ec992fa
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-12 19:13:21 -0400

    update main.py
    turtle race app

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from turtle import Turtle, Screen
 import turtle_blueprint
-screen = Screen()
-screen.screensize(200,200)
 
-# red_turtle = Turtle().color("red")
-# green_turtle = Turtle().color("green")
-# blue_turtle = Turtle().color("blue")
-# purple_turtle = Turtle().color("purple")
-#
+screen = Screen()
+screen.setup(width=500,height=400)
+user_input = screen.textinput(title="Place Your Bets",prompt="Which Turtle will win the Race? Type(red, green or blue): ")
+turtle_colors = ["red","green","purple","yellow","orange","blue"]
+
+hell=Turtle(shape="turtle")
+hell.penup()
+hell.goto(-230,-120)
+
+
+
+
+
+
 
 screen.exitonclick()
 
-#fixes are on the way
--- turtle_blueprint.py
+++ turtle_blueprint.py
@@ -1,10 +1,11 @@
-import turtle
-from turtle import Turtle
-import random
-
-move_forward = random.randint(5,10)
-
-def turtle_move():
-    turtle.forward(move_forward)
-# def turtle_start(start):
-#     turtle.goto(start)
+# from turtle import Turtle
+# import random
+#
+# turtle = Turtle()
+#
+# move_forward = random.randint(5,10)
+#
+# def turtle_move():
+#     turtle.forward(move_forward)
+# # def turtle_start(start):
+# #     turtle.goto(start)
