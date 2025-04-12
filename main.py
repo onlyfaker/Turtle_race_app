140a40f2f89695efcd62402d954dbd11bc248680
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-12 19:58:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 import turtle_blueprint
+import random
 
 screen = Screen()
 screen.setup(width=500,height=400)
@@ -7,13 +8,33 @@ turtle_colors = ["red","green","purple","black","orange","blue"]
 user_input = screen.textinput(title="Place Your Bets",prompt="Which Turtle will win the Race? "
                                                              "\nChoose one of the colors(red,grren,purple,black,orange,blue:")
 
+all_turtles = []
+is_game_on = False
+
 turtle_position = -100
 for color in turtle_colors:
-    hell = Turtle(shape="turtle")
-    hell.color(color)
-    hell.penup()
-    hell.goto(-230, turtle_position)
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(color)
+    new_turtle.penup()
+    new_turtle.goto(-230, turtle_position)
     turtle_position+=40
+    all_turtles.append(new_turtle)
+
+if user_input:
+    is_game_on=True
+
+while is_game_on:
+    for turtle in all_turtles:
+        turtle.speed(6)
+        move_forward = random.randint(0, 10)
+        turtle.forward(move_forward)
+        if turtle.xcor()>=235:
+            is_game_on=False
+            winner = turtle.pencolor()
+            if user_input == winner:
+                print(f"You WON!!!! The winner is {winner}")
+            else:
+                print(f"You LOST! The winner is {winner}")
 
 
 screen.exitonclick()
