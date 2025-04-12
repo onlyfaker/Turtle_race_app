f2dfc806153f18ea1991b7cdca0ecb42c5a84443
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-12 19:19:45 -0400

    update

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import turtle_blueprint
 
 screen = Screen()
 screen.setup(width=500,height=400)
-user_input = screen.textinput(title="Place Your Bets",prompt="Which Turtle will win the Race? Type(red, green or blue): ")
-turtle_colors = ["red","green","purple","yellow","orange","blue"]
+turtle_colors = ["red","green","purple","black","orange","blue"]
+user_input = screen.textinput(title="Place Your Bets",prompt="Which Turtle will win the Race? "
+                                                             "\nChoose one of the colors(red,grren,purple,black,orange,blue:")
 
-hell=Turtle(shape="turtle")
-hell.penup()
-hell.goto(-230,-120)
-
-
-
-
-
+turtle_position = -100
+for color in turtle_colors:
+    hell = Turtle(shape="turtle")
+    hell.color(color)
+    hell.penup()
+    hell.goto(-230, turtle_position)
+    turtle_position+=40
 
 
 screen.exitonclick()
