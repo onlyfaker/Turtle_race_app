617d7d96dc6bfa8414eefa93bcc4f2cb7ad2bb92
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-14 20:36:25 -0400

    fixed an issue
    fixed an issue of a chance of double winner

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import time
 from turtle import Turtle, Screen
 import random
 # todo - make it so multiple people can place bets!
-# todo -maybe add like betting
-# todo - fix an error where 2 colors win
-# todo - add odds feature
 screen = Screen()
 screen.setup(width=500,height=400)
-turtle_colors = ["red","green","purple","black","orange","blue","yellow"]
+turtle_colors = ["red","green","purple","black","orange","blue"]
 user_input = screen.textinput(title="Place Your Bets",prompt="Which Turtle will win the Race? "
-                                                             "\nChoose one of the colors(red,green,purple,black,orange,blue,yellow)")
+                                                             "\nChoose one of the colors(red,green,purple,black,orange,blue)")
 correct_input = True
 # check if the input is valid, as turtle doesn't have window with buttons to select color
 while correct_input:
@@ -35,19 +32,25 @@ for color in turtle_colors:
     all_turtles.append(new_turtle)
 
 is_game_on = True
+winner = None
 #unit we get a winner we race!
 while is_game_on:
     for turtle in all_turtles:
+        if not is_game_on:  # skip remaining turtles if we already have a winner
+            break
         turtle.speed(6)
         move_forward = random.randint(0, 10)
         turtle.forward(move_forward)
-        if turtle.xcor()>=230:
-            is_game_on=False
+
+        if turtle.xcor() >= 230 and winner is None:
             winner = turtle.pencolor()
-            if user_input == winner:
-                print(f"You WON!!!! The winner is {winner}")
-            else:
-                print(f"You LOST! The winner is {winner}")
+            is_game_on = False
+
+if winner:
+    if user_input == winner:
+        print(f"You WON!!!! The winner is {winner}")
+    else:
+        print(f"You LOST! The winner is {winner}")
 
 screen.exitonclick()
 time.sleep(5)
